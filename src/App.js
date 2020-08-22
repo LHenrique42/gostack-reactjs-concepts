bdd84e628af0abd43338ddf0e32e2e7a9834cd38
Henrique <henriquegundes@outlook.com>
2020-08-22 16:02:43 -0300

    Tests passed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    }).catch(err => {
+      console.log(err);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('/repositories', {
+      title: `Desafio React JS ${Date.now()}`,
+      url: "https://github.com/LHenrique42/gostack-reactjs-concepts",
+      techs: ["NodeJS","ReactJS"]
+    }).then(response => {
+      setRepositories([...repositories, response.data]);
+    }).catch(err => {
+      console.log(err);
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`).then(() => {
+      const repositoryIndex = repositories.findIndex(repository => 
+        repository.id === id);
+      const listRepositories = repositories.filter( repository => repository.id !== id );
+      setRepositories(listRepositories);
+    }).catch(err => {
+      console.log(err);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+          {repositories.map(repositorie => {
+            return(
+              <li key={repositorie.id}>
+                <ul>{repositorie.title}</ul>
+                <button onClick={() => handleRemoveRepository(repositorie.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+          }
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
